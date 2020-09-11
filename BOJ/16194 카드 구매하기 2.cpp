3263da73513a633758dc88c767000788b660a752
jaeyong <maxwodyd98@gmail.com>
2020-09-11 17:50:39 +0900

    Upload BOJ Problem: DP

---
+++ BOJ/16194 카드 구매하기 2.cpp
@@ -0,0 +1,38 @@
+//
+//  16194 카드 구매하기 2.cpp
+//  codingtest
+//
+//  Created by 이재용 on 2020/09/11.
+//  Copyright © 2020 jaeyong Lee. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int dp[1001];
+int main(void) {
+    int n;
+    cin >> n;
+    vector<int> v(n+1);
+    for (int i = 1; i <= n; i++) {
+        cin >> v[i];
+    }
+    for (int i = 0; i < 1001; i++) {
+        dp[i] = 10000;
+    }
+    dp[1] = v[1];
+    for (int i = 2; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            if (j == i) {
+                dp[i] = dp[i] < v[j] ? dp[i] : v[j];
+            } else {
+                dp[i] = dp[i] < dp[i-j] + v[j] ? dp[i] : dp[i-j] + v[j];
+            }
+        }
+    }
+    
+    cout << dp[n] << "\n";
+    return 0;
+}
+
+

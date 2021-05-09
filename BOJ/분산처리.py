ccdb9a5e364b69492ed702b8e06f6a86722c732a
jaeyong <maxwodyd98@gmail.com>
2021-05-09 17:16:41 +0900

    백준 파이썬 연습: 분산처리

---
+++ BOJ/분산처리.py
@@ -0,0 +1,26 @@
+
+def lastcomputer(n,k):
+    a = 1
+    if k==0: 
+        k=4
+    for _ in range(k):
+        a*=n
+        a%=10
+    return a
+    
+n=int(input())
+while n:
+    n-=1
+    a,b = map(int, input().split())
+    if a%10==0:
+        print(10)
+    else:
+        temp = b % 4
+        print(lastcomputer(a, temp))
+
+# 5
+# 1 6
+# 3 7
+# 6 2
+# 7 100
+# 9 635

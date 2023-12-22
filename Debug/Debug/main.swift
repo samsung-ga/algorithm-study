4c6b0630d1caaac0d48d60b7dfc3055c11639169
ios-woody <jaeyong.lee@bucketplace.net>
2023-12-22 23:41:23 +0900

    Debug 프로젝트 추가

---
+++ Debug/Debug/main.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+public final class UF {
+    private let n: Int
+    
+    init(n: Int) {
+        self.n = n
+    }
+
+    public func find(p: Int) -> Bool {
+
+    }
+
+    public func countOfConnectedComponents() -> Int {
+
+    }
+
+    public func union(p: Int, q: Int) {
+
+    }
+
+    public func connected(p: Int, q: Int) -> Bool {
+
+    }
+}
